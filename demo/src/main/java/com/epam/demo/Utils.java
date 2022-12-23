24f6b1efaa1d4ca6e78e941bfb9d1a848fce847a
SardorAKayumov <sardor.zeroz@gmail.com>
2022-12-23 14:39:46 +0500

    commit

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,23 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        try {
+            if (args == null)
+                return false;
+            for (String str : args) {
+                if (str == null)
+                    return false;
+                if (str.trim().isEmpty())
+                    return false;
+                if(str.charAt(0) == '0')
+                    return false;
+                if (Double.parseDouble(str) <= 0)
+                    return false;
+            }
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+        return false;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,17 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        try {
+            if (str == null)
+                return false;
+            if (str.trim().isEmpty())
+                return false;
+            if(str.charAt(0) == '0')
+                return false;
+            return Double.parseDouble(str) > 0;
+        } catch(NumberFormatException e) {
+            System.out.println(e);
+        }
+        return false;
     }
 }
